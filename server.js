698159de566cf95d35a307efd71874339f27d316
Ashley Evans <ashley_e@live.com>
2019-07-19 15:51:16 -0700

    Removed yarn.lock, updated package.json and updated connection information on server.js

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
-const http = require('http');
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 5000;
 
 require('dotenv').config();
 
-app.get('/', function(req, res) {
-  res.status(200).send('Hello world');
+app.get('/', (req, res) => {
+  res.send('Hello world');
 });
 
-app.listen(PORT, function() {
-  console.log('Server is running on PORT:',PORT);
+app.listen(PORT, () => {
+  console.log('Server is running on PORT:', PORT);
 });
