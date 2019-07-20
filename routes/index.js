40be142cd8a3b632fc68182780d59ed5ec132d9f
Ashley Evans <ashley_e@live.com>
2019-07-19 18:43:02 -0700

    Cleaned up index.js as the authentication routes are in a separate file now.

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,6 @@
-import passport from 'passport';
 const path = require("path");
 const router = require("express").Router();
 const apiRoutes = require("./api");
-
-/* GET Google Authentication API. */
-router.get(
-  "/auth/google",
-  passport.authenticate("google", { scope: ["profile", "email"] })
-);
-router.get(
-  "/auth/google/callback",
-  passport.authenticate("google", { failureRedirect: "/", session: false }),
-  function(req, res) {
-      var token = req.user.token;
-      res.redirect("http://localhost:3000?token=" + token);
-  }
-);
 
 // API Routes
 router.use("/api", apiRoutes);
