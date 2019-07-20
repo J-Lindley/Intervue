029528d7b1a1c56e94a22ff5d4f4b14e0fc52818
JLindley <jonlindley@gmail.com>
2019-07-20 13:27:01 -0700

    working on merging

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
-const mongoose = require('mongoose');
+
 
 
 // Define middleware here
