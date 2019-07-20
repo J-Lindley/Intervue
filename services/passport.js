de885760f21e4b402fa1acb8d0ae50767d5d587c
Ashley Evans <ashley_e@live.com>
2019-07-19 20:31:44 -0700

    Updating passport.js to serialize and deserialize user.

--- services/passport.js
+++ services/passport.js
@@ -4,6 +4,16 @@ const mongoose = require("mongoose");
 const keys = require("../config/keys");
 const User = mongoose.model("user");
 
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then(user => {
+    done(null, user);
+  });
+});
+
 // Using passport to set up new GoogleStrategy and begin authentication.
 passport.use(
   new GoogleStrategy(
