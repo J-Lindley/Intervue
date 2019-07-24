67e6a04fc8b005bc3c716dabc7d471ffc7ab34a6
buley004 <47130556+buley004@users.noreply.github.com>
2019-07-23 19:25:29 -0700

    Delete keys.js

--- config/keys.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  googleClientID: '76901378445-plqcfai6afvc6krl1l29v2fk3riintkr.apps.googleusercontent.com',
-  googleClientSecret: 'W13ydDn1tCpzNHhhaoouJP7L',
-  cookieKey: 'intervuekey'
-};
