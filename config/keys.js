e0eb38822128a25bfa38830dfe2904a2ccde13ac
LahRee <Laurie.Strecker@gmail.com>
2019-07-23 19:32:29 -0700

    npm i extras

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  googleClientID: '76901378445-plqcfai6afvc6krl1l29v2fk3riintkr.apps.googleusercontent.com',
+  googleClientSecret: 'W13ydDn1tCpzNHhhaoouJP7L',
+  cookieKey: 'intervuekey'
+};
