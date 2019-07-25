862bdf97cfa7e3d2bfd50f58f92bb9d19e5c7406
buley004 <colinbuley@D-10-18-244-49.dhcp4.washington.edu>
2019-07-24 20:40:09 -0700

    refactored with dotenv

--- services/passport.js
+++ services/passport.js
@@ -1,7 +1,7 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const mongoose = require("mongoose");
-const keys = require("../config/keys");
+// const keys = require("../config/keys");
 const User = mongoose.model("user");
 
 passport.serializeUser((user, done) => {
@@ -18,8 +18,8 @@ passport.deserializeUser((id, done) => {
 passport.use(
   new GoogleStrategy(
     {
-      clientID: keys.googleClientID,
-      clientSecret: keys.googleClientSecret,
+      clientID: process.env.googleClientID,
+      clientSecret: process.env.googleClientSecret,
       callbackURL: "/auth/google/callback"
     },
     (accessToken, refreshToken, profile, done) => {
