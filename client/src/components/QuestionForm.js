ab8974305f1698f9acae60b54a504633155d2a0b
Ashley Evans <ashley_e@live.com>
2019-08-02 12:21:17 -0700

    Adding formatting to new question page and new component for welcome message

--- client/src/components/QuestionForm.js
+++ client/src/components/QuestionForm.js
@@ -1,9 +1,9 @@
-import React , {Component} from "react";
+import React, { Component, Fragment } from "react";
 import "../App.css";
-import axios from 'axios';
+import axios from "axios";
+import NewQuestionWelcome from "./NewQuestionWelcome";
 
 class QuestionForm extends Component {
-
   state = {
     question: "",
     answer: "",
@@ -18,44 +18,59 @@ class QuestionForm extends Component {
 
   saveMyQuestion = () => {
     axios.post("api/user/savedQuestions");
-  }
+  };
 
   render() {
-    return ( 
-      <div className="ui container">
-            <form className="ui form">
-                <div className="field">
-                  <label className="formLabel">Question: </label>
-                  <textarea placeholder="Please submit the question here. (Required) " type="text"></textarea>
-                </div>
-                <div className="field">
-                  <label className="formLabel">Answer: </label>
-                  <textarea placeholder="Please submit the question answer here. (Required) " type="text"></textarea>
-                </div>
-                <div className="field">
-                  <label className="formLabel">Company: </label>
-                  <input placeholder="Company (if applicable)" type="text"></input>
-                </div>
-                <div className="field">
-                  <label className="formLabel">Question Category: </label>
-                  <select className="ui search dropdown">
-                    <option value="">Select Category (Required)</option>
-                    <option value="javascript">Javascript</option>
-                    <option value="behavioral">Behavioral</option>
-                    <option value="node">Node</option>
-                    <option value="react">React</option>
-                    <option value="express">Express</option>
-                    <option value="algorthims">Algorthims</option>
-                    <option value="AO"></option>
-                    <option value="AI">Anguilla</option>
-                    <option value="AQ">Antarctica</option>
-                    <option value="AG">Antigua and Barbuda</option>
-                    <option value="AR">Argentina</option>
-                  </select>
-                </div>
-                <div className="ui submit button">Submit Question</div>
-            </form> 
+    return (
+      <Fragment>
+        <div className="ui container">
+          <div className="ui tabular menu">
+            <NewQuestionWelcome />
+          </div>
+          <div class="ui bottom attached segment">
+              <form className="ui form">
+              <div className="field">
+                <label className="formLabel">Question: </label>
+                <textarea
+                  placeholder="Please submit the question here. (Required) "
+                  type="text"
+                />
+              </div>
+              <div className="field">
+                <label className="formLabel">Answer: </label>
+                <textarea
+                  placeholder="Please submit the question answer here. (Required) "
+                  type="text"
+                />
+              </div>
+              <div className="field">
+                <label className="formLabel">Company: </label>
+                <input placeholder="Company (if applicable)" type="text" />
+              </div>
+              <div className="field">
+                <label className="formLabel">Question Category: </label>
+                <select className="ui search dropdown">
+                  <option value="">Select Category (Required)</option>
+                  <option value="javascript">Javascript</option>
+                  <option value="behavioral">Behavioral</option>
+                  <option value="node">Node</option>
+                  <option value="react">React</option>
+                  <option value="express">Express</option>
+                  <option value="algorthims">Algorthims</option>
+                  <option value="AO" />
+                  <option value="AI">Anguilla</option>
+                  <option value="AQ">Antarctica</option>
+                  <option value="AG">Antigua and Barbuda</option>
+                  <option value="AR">Argentina</option>
+                </select>
+              </div>
+              <div className="ui submit button">Submit Question</div>
+            </form>
+          </div>
         </div>
+
+        
+      </Fragment>
     );
   }
 }
--- client/src/pages/ProfilePage.js
+++ client/src/pages/ProfilePage.js
@@ -10,18 +10,15 @@ class Profile extends Component {
     return (
       <Fragment>
         <div className="ui container">
-            <div className="twelve wide column">
-            <WelcomeUser />
-            </div>
             <div className="ui tabular menu">
-              <a className="item">Saved Questions</a>
-              <a className="item">Submitted Questions</a>
+            <WelcomeUser />
             </div>
             <div class="ui bottom attached segment">
                 <SavedQuestions />
             </div>
-            <SubmittedQuestions />
-            
+            <div class="ui bottom attached segment">
+              <SubmittedQuestions />
+            </div> 
         </div>
       </Fragment>
     );
