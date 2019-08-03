16f724586e6268cb6b263823aa063094bee5d847
Ashley Evans <ashley_e@live.com>
2019-08-03 13:38:40 -0700

    Committing before pulling from master

--- client/src/components/SubmittedQuestions.js
+++ client/src/components/SubmittedQuestions.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment }from 'react';
+import React, { Component }from 'react';
 import '../App.css';
 import axios from 'axios';
 
@@ -25,26 +25,27 @@ class SubmittedQuestions extends Component {
 
   render() {
     return (
-      <Fragment>
-      <h3 className="ui header" id="submittedQuestionTitle">Submitted Questions</h3>
-        {this.state.submittedQuestions.submitted.length ? this.state.submittedQuestions.submitted
-        .map(submittedQuestion => (
-          <div className="ui segment" id="submittedQuestionContainer">
-            <div className="submittedQuestionDetails">
-              <h3>Question Category: </h3> 
-              <strong>{submittedQuestion.questionType}</strong>
-            </div>
-            <div className="submittedQuestionDetails">
-              <h3>Question: </h3> 
-              <strong>{submittedQuestion.question}</strong>
-            </div>
-            <div className="submittedQuestionDetails">
-              <h3>Answer: </h3> 
-              <strong>{submittedQuestion.answer}</strong>
-            </div>
-          </div>
-        )) : ""}
-      </Fragment>
+      <div>Hello</div>
+      // <Fragment>
+      // <h3 className="ui header" id="submittedQuestionTitle">Submitted Questions</h3>
+      //   {this.state.submittedQuestions.submitted.length ? this.state.submittedQuestions.submitted
+      //   .map(submittedQuestion => (
+      //     <div className="ui segment" id="submittedQuestionContainer">
+      //       <div className="submittedQuestionDetails">
+      //         <h3>Question Category: </h3> 
+      //         <strong>{submittedQuestion.questionType}</strong>
+      //       </div>
+      //       <div className="submittedQuestionDetails">
+      //         <h3>Question: </h3> 
+      //         <strong>{submittedQuestion.question}</strong>
+      //       </div>
+      //       <div className="submittedQuestionDetails">
+      //         <h3>Answer: </h3> 
+      //         <strong>{submittedQuestion.answer}</strong>
+      //       </div>
+      //     </div>
+      //   )) : ""}
+      // </Fragment>
     )
   }
 }
