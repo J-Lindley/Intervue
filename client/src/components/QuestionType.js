1dc19d9500c2b3c1aac2b594c8646395bceead1e
Ashley Evans <ashley_e@live.com>
2019-08-05 19:20:14 -0700

    Adding click event to save button on Category Page

--- client/src/components/QuestionType.js
+++ client/src/components/QuestionType.js
@@ -1,10 +1,37 @@
 import React, {PureComponent, Fragment}from 'react';
 import '../App.css';
+import axios from 'axios';
 import { connect } from "react-redux";
 import Slider from 'react-slick';
 import SaveBtn from './SaveBtn';
 
 class QuestionType extends PureComponent {
+
+  state = {
+    questions: [],
+    userId: "null"
+  }
+
+  componentDidMount() {
+    axios.get("/api/questions/")
+    .then(res => {
+      this.setState({questions: res.data})}).catch(err => console.log(err));  
+    }
+
+  saveQuestion = id => {
+    //first get and store user id
+    axios.get("/api/current_user")
+    .then(res => {
+      console.log(res.data.googleId);
+      console.log(id);
+      
+      let user = res.data.googleId;
+      axios.put("api/user/saveQuestion", {qid: id, uid: user})
+        .then(res => {
+          console.log(res);
+        })
+    });
+  }
 
   render() {
     const settings = {
@@ -24,7 +51,7 @@ class QuestionType extends PureComponent {
                 <h3>{question.question}</h3>
                 <h2 className="questionLabel"> Answer </h2>
                 <h3>{question.answer}</h3>
-                <SaveBtn />
+                <SaveBtn qid={question._id} onClick={ () => this.saveQuestion(question._id)}/>
                 <div className="extra content">
                   <span className="left floated" id="thumbsUp">
                   <i className="thumbs up outline icon"></i>
